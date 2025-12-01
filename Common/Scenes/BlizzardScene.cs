67f099901bed4dc7b484eb2101c204ae635b603f
GabeHasWon <gabehw.contact@gmail.com>
2025-11-30 22:50:31 -0600

    1.0.0.9

--- Common/Scenes/BlizzardScene.cs
+++ Common/Scenes/BlizzardScene.cs
@@ -6,10 +6,10 @@ internal class BlizzardScene : ModSceneEffect
     public override SceneEffectPriority Priority => SceneEffectPriority.BiomeMedium;
 
     public override bool IsSceneEffectActive(Player player) => ModContent.GetInstance<MusicConfig>().Blizzard
-            && player.ZoneSnow
-            && player.ZoneOverworldHeight
-            && !player.ZoneCorrupt
-            && !player.ZoneMeteor
-            && !player.ZoneCrimson
-            && Main.raining;
+        && player.ZoneSnow
+        && player.ZoneOverworldHeight
+        && !player.ZoneCorrupt
+        && !player.ZoneMeteor
+        && !player.ZoneCrimson
+        && Main.raining;
 }
