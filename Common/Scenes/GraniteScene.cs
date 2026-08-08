cee59c1ca6a7ea9fd760516ad75d83de6ddb1c5c
GabeHasWon <gabehw.contact@gmail.com>
2026-08-08 13:07:12 -0500

    1.0.0.14

--- Common/Scenes/GraniteScene.cs
+++ Common/Scenes/GraniteScene.cs
@@ -7,18 +7,16 @@ internal class GraniteScene : ModSceneEffect
 {
     public override int Music => MusicLoader.GetMusicSlot(Mod, "Sounds/Music/Granite");
     public override SceneEffectPriority Priority => SceneEffectPriority.BiomeMedium;
-    public override bool IsSceneEffectActive(Player player) => MusicConfig.Instance.Granite && ModContent.GetInstance<GraniteTileCounter>().InGranite;
+    public override bool IsSceneEffectActive(Player player) => MusicConfig.Instance.Granite && Main.tile[player.Center.ToTileCoordinates16()].WallType == WallID.GraniteUnsafe;
+    //ModContent.GetInstance<GraniteTileCounter>().InGranite;
 }
 
+[Obsolete("Replaced with background wall checks, but may be used in the future?")]
 public class GraniteTileCounter : ModSystem
 {
     public bool InGranite => _graniteCount > 400;
 
     private int _graniteCount = 0;
 
-    public override void TileCountsAvailable(ReadOnlySpan<int> tileCounts)
-    {
-        _graniteCount = tileCounts[TileID.Granite];
-
-    }
+    public override void TileCountsAvailable(ReadOnlySpan<int> tileCounts) => _graniteCount = tileCounts[TileID.Granite];
 }
--- Common/Scenes/MarbleScene.cs
+++ Common/Scenes/MarbleScene.cs
@@ -7,9 +7,10 @@ internal class MarbleScene : ModSceneEffect
 {
     public override int Music => MusicLoader.GetMusicSlot(Mod, "Sounds/Music/MarbleBiome");
     public override SceneEffectPriority Priority => SceneEffectPriority.BiomeMedium;
-    public override bool IsSceneEffectActive(Player player) => MusicConfig.Instance.Marble && ModContent.GetInstance<MarbleTileCounter>().InMarble;
+    public override bool IsSceneEffectActive(Player player) => MusicConfig.Instance.Marble && Main.tile[player.Center.ToTileCoordinates16()].WallType == WallID.MarbleUnsafe;
 }
 
+[Obsolete("Replaced with background wall checks, but may be used in the future?")]
 public class MarbleTileCounter : ModSystem
 {
     public bool InMarble => _marbleCount > 200;
